442dc7ce2f8e07ce1868fcd9ce0ad3dac932be8f
peakXD <rasravee.20@gmail.com>
2025-07-30 21:50:55 +0700

    20250730

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is grading program
+
+input = int(input("Enter you grade: "))
+
+if input >= 80:
+    print("A")
+elif input >= 75:
+    print("B+")
+elif input >= 70:
+    print("B")
+elif input >= 65:
+    print("C+")
+elif input >= 60:
+    print("C")
+elif input >= 55:
+    print("D+")
+elif input >= 50:
+    print("D")
+else:
+    print("F")
