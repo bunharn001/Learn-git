f8af9592c22802628ffdbb217302839d214639ef
bunharn001 <ss7902712@gmail.com>
2025-07-30 21:23:28 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is grading program
+
+input = int(input("Enter you grade: "))
+
+if input >= 90:
+    print("A")
+elif input >= 80:
+    print("B")
+elif input >= 70:
+    print("C")
+elif input >= 60:
+    print("D")
+else:
+    print("F")
