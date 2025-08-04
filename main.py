db195cbdd0d17b44f2fa1c37c6837263c696dfe6
Thitisak <thitisak.phu@rmuttoac.ac.th>
2025-08-04 23:37:34 +0700

    Add B+

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ print("hello World")
 
 input = int(input("Enter grade score: "))
 
-if input >= 90:
+if input >= 80:
     print("A")
-elif input >= 80:
-    print("B")
+elif input >= 75:
+    print("B+")
 elif input >= 70:
-    print("C")
+    print("B")
+elif input >= 65:
+    print("C+")
 elif input >= 60:
+    print("C")
+elif input >= 55:
+    print("D+")
+elif input >= 50:
     print("D")
 else:
     print("F")
