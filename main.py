630ac9071226a037369464d07a2f742db256163b
bunharn001 <ss7902712@gmail.com>
2025-08-04 19:25:47 +0700

    20250804 Create frontend

---
+++ main.py
@@ -0,0 +1,14 @@
+print("hello World")
+
+input = int(input("Enter grade score: "))
+
+if input >= 90:
+    print("A")
+elif input >= 80:
+    print("B")
+elif input >= 70:
+    print("C")
+elif input >= 60:
+    print("D")
+else:
+    print("F")
